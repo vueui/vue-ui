83cb67609f3fcb3426241a35084735e0a80921d3
Agon Bina <agon_bina@hotmail.com>
2014-11-09 06:25:47 +0100

    implemented a mixin which allows binding SemanticUI behavior of a module to a VueUI component with before/after hooks

--- helpers/mixins/bindBehaviors.js
+++ helpers/mixins/bindBehaviors.js
@@ -1,13 +1,31 @@
 
 
 module.exports = {
-    beforeCompile: function () {
+    compiled: function () {
         var vm = this;
-        var behaviors = this.$options.behaviors || [];
+        var behaviors = this.$options.behaviors || {};
         var moduleName = this.$options.name.toLowerCase();
+        var $element = $(this.$$[moduleName]);
 
-        behaviors.forEach(function (behavior) {
-            vm['$' + behavior] = $(vm.$el)[moduleName].bind($(vm.$el), behavior);
-        });
+        // Register the behavior(method) as $behavior on the vm with before/after hooks
+        $.each(behaviors, register);
+
+        function register(behavior, settings) {
+            var after = $.isFunction(settings.after) ? settings.after.bind(vm) : $.noop;
+            var method = $element[moduleName].bind($element, behavior);
+            var before = $.isFunction(settings.before) ? settings.before.bind(vm) : $.noop;
+
+            vm['$' + behavior] = function() {
+                var args = arguments;
+                var done = function () {
+                    method.apply($element, args);
+                    after();
+                };
+
+                if (before !== $.noop) before(done);
+                else done();
+            }
+        }
+
     }
 };
